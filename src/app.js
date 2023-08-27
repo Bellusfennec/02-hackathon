02239ede29c3b9b7bff9e2eb8acf568364343f83
Bellusfennec <bellusfennec@gmail.com>
2023-08-27 16:58:13 +0300

    fix: :bug: fix favorite provider

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import AppLoader from "./common/hoc/appLoader";
+import FavoritesProvider from "./hooks/useFavorites";
 import AppRoutes from "./routes";
 
 function App() {
   return (
     <div className="bg-gray-50 dark:bg-gray-300">
-      <AppLoader>
-        <AppRoutes />
-      </AppLoader>
+      <FavoritesProvider>
+        <AppLoader>
+          <AppRoutes />
+        </AppLoader>
+      </FavoritesProvider>
     </div>
   );
 }
