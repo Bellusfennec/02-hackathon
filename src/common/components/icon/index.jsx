fefa0b25c0deba138c5ea35c8eafd49544f771e1
bellusfennec <bellusfennec@gmail.com>
2023-08-28 09:26:34 +0300

    refactor:

--- src/common/components/icon/index.jsx
+++ src/common/components/icon/index.jsx
@@ -4,7 +4,7 @@ import sprite from "../../../img/sprite.svg";
 
 const Icon = ({ name, className }) => (
   <svg className={className}>
-    <use xlinkHref={`${sprite}/#${name}`} />
+    <use xlinkHref={`.${sprite}/#${name}`} />
   </svg>
 );
 
